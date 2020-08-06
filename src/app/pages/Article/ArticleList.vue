bcb651f4a5aaee7f584990177ce39416d5b69742
marvin <marvin@marvinisaac.com>
2020-08-06 11:43:35 +0800

    :construction: Loading component for article list view

--- src/app/pages/Article/ArticleList.vue
+++ src/app/pages/Article/ArticleList.vue
@@ -1,10 +1,36 @@
 <template>
     <div class="container-article-list">
         <div class="container">
-            <h1 class="title">Home</h1>
+            <loading v-if="this.postList === undefined"
+                :message="`Wait lang...`">
+            </loading>
+
+            <article-list-card
+                v-for="(post, index) in postList"
+                :key="index"
+                :post="post"
+                :postTitleSlug="index">
+            </article-list-card>
         </div>
     </div>
 </template>
+
+<script>
+import ArticleListCard from './ArticleListCard.vue'
+import Loading from './../../partials/Loading.vue'
+
+export default {
+    components: {
+        ArticleListCard,
+        Loading
+    },
+    computed: {
+        postList: function () {
+            return this.$store.state.posts || undefined
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .container-article-list {
---
+++ src/app/pages/Article/ArticleListCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-article-list-card">
+        <router-link :to="`/${postTitleSlug}`">
+            <h1 class="title">
+                {{ post.title }}
+            </h1>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: Object,
+        postTitleSlug: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.container-article-list-card {
+    margin-bottom: auto;
+    width: 100%;
+}
+</style>
